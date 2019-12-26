595e6196a563a3dc777eb5dc2989f0a9975b8a0f
koussay-dellai <koussaydellai22@gmail.com>
2019-12-26 01:36:11 +0100

    listing arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if (len(sys.argv) <= 1):
         print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+        if (len(sys.argv) == 2):
+            print("1 argument:")
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))
