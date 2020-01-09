6dd52efb6e62d1498dbfec1374d33ce073c35807
koussay-dellai <koussaydellai22@gmail.com>
2020-01-09 01:45:39 +0100

    execept pass

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elements = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+    except IndexError:
+        pass
+    print()
+    return elements
