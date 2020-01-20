11e4920c94cf99893364a230430172cf3d905ab7
koussay-dellai <koussaydellai22@gmail.com>
2020-01-20 10:28:03 +0100

    fix

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width = 0, height = 0):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if (self.width == 0) or (self.height == 0):
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if (self.width == 0) or (self.height == 0):
+            return " "
+        else:
+            for i in range(self.height):
+                for j in range(self.width):
+                    return "#"
+                print()
+        
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
