56ac9526f00e8c9bb0841dee6aba590dcc68894d
koussay-dellai <koussaydellai22@gmail.com>
2020-01-22 23:38:18 +0100

    fix pycode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+
+"""
+module to read and print the content of a file
+"""
+
+
 def read_file(filename=""):
+    """
+    Function to print on stdout
+    """
     with open(filename, encoding='UTF8') as f:
         print(f.read(), end="")
