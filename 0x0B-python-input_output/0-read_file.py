5fbb92f7f8220869a2299e658e061509bfe8dfc9
koussay-dellai <koussaydellai22@gmail.com>
2020-01-22 23:32:46 +0100

    pyc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 def read_file(filename=""):
-    with open(filename, encoding = 'UTF8') as f:
-        print(f.read(), end ="")
+    with open(filename, encoding='UTF8') as f:
+        print(f.read(), end="")
