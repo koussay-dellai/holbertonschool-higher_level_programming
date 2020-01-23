b817d0463f467a3adccd266e8922f5d5842d2190
koussay-dellai <koussaydellai22@gmail.com>
2020-01-23 14:43:02 +0100

    read number of lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -12,8 +12,6 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="UTF8") as f:
         for i in f:
             number += 1
-        if (nb_lines <= 0) or (nb_lines >= number):
-            print(f.read(), end="")
-        else:
-            for x in range(nb_lines):
-                print(f.readline(), end="")
+            print(i, end="")
+            if nb_lines == number:
+                break
