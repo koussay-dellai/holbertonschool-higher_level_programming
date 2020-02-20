c1f4d537c78273a5af1e322f8e2a0a383a0bf581
koussay-dellai <koussaydellai22@gmail.com>
2020-02-20 23:46:23 +0100

    import functions

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    dict1 = {'+': add, '-': sub, '*': mul, '/': div}
+    for key, value in dict1.items():
+        if argv[2] == key:
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+                                         value(int(argv[1]), int(argv[3]))))
+            exit(0)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
