51ec404eb2a313c678e7a715299cd0af6e9ec285
koussay-dellai <koussaydellai22@gmail.com>
2020-02-20 23:52:41 +0100

    import functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,13 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    dict1 = {'+': add, '-': sub, '*': mul, '/': div}
-    for key, value in dict1.items():
-        if argv[2] == key:
-            print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3],
-                                         value(int(argv[1]), int(argv[3]))))
-            exit(0)
+    else:
+        dict1 = {'+': add, '-': sub, '*': mul, '/': div}
+        for key, value in dict1.items():
+            if argv[2] == key:
+                print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3],
+                                                     value(int(argv[1]),
+                                                           int(argv[3]))))
+                exit(0)
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
