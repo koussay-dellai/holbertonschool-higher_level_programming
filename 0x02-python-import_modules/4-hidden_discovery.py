c79c928fe30a007cb2abbcd5c105253108e859c9
koussay-dellai <koussaydellai22@gmail.com>
2020-02-20 23:12:05 +0100

    ddir built in

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for i in(dir(hidden_4)):
-        out = "__"
-        if out not in i:
-            print("{}".format(i))
+    import hidden_4
+    list = [name for name in dir(hidden_4) if "__" not in name]
+    for i in range(len(list)):
+        print(list[i])
